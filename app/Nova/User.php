3990228e84a18d645a8540913562f607099197f0
vadim.ustinovich <vadim.ustinovich@softlex.pro>
2020-12-22 18:35:34 +0300

    WIP:PWW-39 create new column, nova-resource and seeder for users

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -3,7 +3,8 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\Avatar;
+use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
@@ -44,11 +45,11 @@ class User extends Resource
         return [
             ID::make()->sortable(),
 
-            Gravatar::make()->maxWidth(50),
-
             Text::make('Name')
                 ->sortable()
                 ->rules('required', 'max:255'),
+
+            Avatar::make('Photo', 'image')->path('/photos'),
 
             Text::make('Email')
                 ->sortable()
@@ -60,6 +61,9 @@ class User extends Resource
                 ->onlyOnForms()
                 ->creationRules('required', 'string', 'min:8')
                 ->updateRules('nullable', 'string', 'min:8'),
+
+            BelongsTo::make('Роль', 'role', Role::class)
+                ->rules('required'),
         ];
     }
 
