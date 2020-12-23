0ddb4b206afee1f48743f9441b93802c65485d4f
vadim.ustinovich <vadim.ustinovich@softlex.pro>
2020-12-23 10:50:31 +0300

    PWW-39 add new item in user seeder, create policy for users and roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function role ()
+    public function role()
     {
         return $this->belongsTo(Role::class, 'role_id');
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,12 +15,22 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Admin',
-            'role_id' => 1,
-            'email' => 'admin@admin.com',
-            'password' => bcrypt(12345),
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
+            [
+                'name' => 'Admin',
+                'role_id' => 1,
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Moderator',
+                'role_id' => 2,
+                'email' => 'moderator@moderator.com',
+                'password' => bcrypt('moderator'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
         ]);
     }
 }
