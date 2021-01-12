a19d1a6f6e7551d482221abe352fd1dff40aa159
alexandr.intern <alexandr.intern@softlex.pro>
2021-01-12 14:12:18 +0300

    PWW-63   Review

--- app/Nova/Country.php
+++ app/Nova/Country.php
@@ -21,7 +21,7 @@ class Country extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'title';
 
     /**
      * The columns that should be searched.
