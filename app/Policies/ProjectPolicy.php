fdc860faa849d5c3d3014fbc1d75ad225cf4a641
alexandr.intern <alexandr.intern@softlex.pro>
2021-01-15 17:10:30 +0300

    PWW-94  The fix for review
    Fixed Policy

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -10,7 +10,7 @@ class ProjectPolicy extends DefaultPolicy
 {
     public function view(User $user)
     {
-        if (($user->role->id='1') or ($user->role->id='2')){
+        if (($user->role->id=='1') or ($user->role->id=='2')){
          return true;
         }
         return false;
