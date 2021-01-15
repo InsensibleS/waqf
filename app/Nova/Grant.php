4d22c1123f05ad877b4fcbce1fb5a9c6537f9880
vadim.ustinovich <vadim.ustinovich@softlex.pro>
2021-01-15 18:29:13 +0300

    PWW-86 make validation dates for Grant

--- app/Nova/Grant.php
+++ app/Nova/Grant.php
@@ -8,7 +8,8 @@ use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Date;
-use Laravel\Nova\Http\Requests\NovaRequest;
+use App\Rules\ValidLateDate;
+use App\Rules\ValidEqualDate;
 
 class Grant extends Resource
 {
@@ -64,51 +65,135 @@ class Grant extends Resource
 
             Date::make('Start of grant', 'start_date')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidLateDate(
+                        date('Y-m-d'),
+                        $request->start_date,
+                        "The start date of the grant cannot be earlier than tomorrow's date.",
+                    )
+                ),
 
             Date::make('End of project selection', 'end_selection_projects')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidEqualDate(
+                        $request->start_date,
+                        $request->end_selection_projects,
+                        'The end date of the project selection cannot be earlier than the start of the grant.',
+                    )
+                ),
 
             Date::make('Start moderation', 'start_moderation')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidLateDate(
+                        $request->end_selection_projects,
+                        $request->start_moderation,
+                        "The moderation start date must be later than the end of the project selection period.",
+                    )
+                ),
 
             Date::make('End moderation', 'end_moderation')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidEqualDate(
+                        $request->start_moderation,
+                        $request->end_moderation,
+                        'The end date of moderation cannot be earlier than the start date of moderation.',
+                    )
+                ),
 
             Date::make('Start of qualification', 'start_qualification')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidLateDate(
+                        $request->end_moderation,
+                        $request->start_qualification,
+                        "Qualification start date must be later than the end of project moderation.",
+                    )
+                ),
 
             Date::make('End of qualification', 'end_qualification')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidEqualDate(
+                        $request->start_qualification,
+                        $request->end_qualification,
+                        'The end date of the qualification cannot be earlier than the start of the qualification.',
+                    )
+                ),
 
             Date::make('Semifinal start', 'start_semifinal')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidLateDate(
+                        $request->end_qualification,
+                        $request->start_semifinal,
+                        "The start of the semi-final must be later than the end of the qualification.",
+                    )
+                ),
 
             Date::make('End of semi-final', 'end_semifinal')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidEqualDate(
+                        $request->start_semifinal,
+                        $request->end_semifinal,
+                        'The end date of the semi-final cannot be earlier than the start date of the semi-final.',
+                    )
+                ),
 
             Date::make('Finals start', 'start_final')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidLateDate(
+                        $request->end_semifinal,
+                        $request->start_final,
+                        "The beginning of the final must be later than the end of the semi-final.",
+                    )
+                ),
 
             Date::make('End of final', 'end_final')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidEqualDate(
+                        $request->start_final,
+                        $request->end_final,
+                        'The end date of the final cannot be earlier than the start date of the final.',
+                    )
+                ),
 
             Date::make('Start of summarizing', 'start_summarizing')
                 ->sortable()
-                ->rules('required'),
+                ->rules(
+                    'required',
+                    new ValidLateDate(
+                        $request->end_final,
+                        $request->start_summarizing,
+                        "The start of summing up must be later than the end of the final.",
+                    )
+                ),
 
             Date::make('End of grant', 'end_date')
                 ->sortable()
-                ->rules('required')
+                ->rules(
+                    'required',
+                    new ValidEqualDate(
+                        $request->start_summarizing,
+                        $request->end_date,
+                        'The end date of the grant cannot be earlier than the start date of the summing up.',
+                    )
+                )
                 ->readonly(function() {
                     return $this->id === 1 ? true : false;
                 }),
@@ -120,7 +205,7 @@ class Grant extends Resource
                 ->withoutTrashed()
                 ->withMeta(['extraAttributes' => [
                     'readonly' => true
-                ]]),
+            ]]),
         ];
     }
 
