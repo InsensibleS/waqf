5fb8c893b00ce00784a3b1fcca6e284b5d961d62
alexandr.intern <alexandr.intern@softlex.pro>
2021-01-19 10:57:27 +0300

    PWW-80    Add seed fou project_hashtag table

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -201,6 +201,7 @@ According to forecasts of the International Renewable Energy Agency (IRENA), as 
         ]);
 
         DB::table('hashtag_project')->insert([
+            //The first Project(id)
             [
                 'hashtag_id' => '1',
                 'project_id' => '1'
@@ -212,6 +213,110 @@ According to forecasts of the International Renewable Energy Agency (IRENA), as 
             [
                 'hashtag_id' => '3',
                 'project_id' => '1'
+            ],
+            //The second Project(id)
+            [
+                'hashtag_id' => '4',
+                'project_id' => '2'
+            ],
+            [
+                'hashtag_id' => '5',
+                'project_id' => '2'
+            ],
+            [
+                'hashtag_id' => '1',
+                'project_id' => '2'
+            ],
+            //The third Project(id)
+            [
+                'hashtag_id' => '2',
+                'project_id' => '3'
+            ],
+            [
+                'hashtag_id' => '3',
+                'project_id' => '3'
+            ],
+            [
+                'hashtag_id' => '4',
+                'project_id' => '3'
+            ],
+            //The fourth  Project(id)
+            [
+                'hashtag_id' => '5',
+                'project_id' => '4'
+            ],
+            [
+                'hashtag_id' => '1',
+                'project_id' => '4'
+            ],
+            [
+                'hashtag_id' => '2',
+                'project_id' => '4'
+            ],
+            //The fifth Project(id)
+            [
+                'hashtag_id' => '3',
+                'project_id' => '5'
+            ],
+            [
+                'hashtag_id' => '4',
+                'project_id' => '5'
+            ],
+            [
+                'hashtag_id' => '5',
+                'project_id' => '5'
+            ],
+            //The sixth Project(id)
+            [
+                'hashtag_id' => '1',
+                'project_id' => '6'
+            ],
+            [
+                'hashtag_id' => '2',
+                'project_id' => '6'
+            ],
+            [
+                'hashtag_id' => '3',
+                'project_id' => '6'
+            ],
+            //The seventh Project(id)
+            [
+                'hashtag_id' => '4',
+                'project_id' => '7'
+            ],
+            [
+                'hashtag_id' => '5',
+                'project_id' => '7'
+            ],
+            [
+                'hashtag_id' => '1',
+                'project_id' => '7'
+            ],
+            //The eighth Project(id)
+            [
+                'hashtag_id' => '2',
+                'project_id' => '8'
+            ],
+            [
+                'hashtag_id' => '3',
+                'project_id' => '8'
+            ],
+            [
+                'hashtag_id' => '4',
+                'project_id' => '8'
+            ],
+            //The ninth Project(id)
+            [
+                'hashtag_id' => '5',
+                'project_id' => '9'
+            ],
+            [
+                'hashtag_id' => '1',
+                'project_id' => '9'
+            ],
+            [
+                'hashtag_id' => '2',
+                'project_id' => '9'
             ],
         ]);
 
