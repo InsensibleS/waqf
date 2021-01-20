fb5bc2ed01af96195762d895dd0691b7853cb614
vadim.ustinovich <vadim.ustinovich@softlex.pro>
2021-01-20 17:31:51 +0300

    PWW-82 add stage for grants

--- app/Http/Resources/GrantsAndProjectsForProfileResource.php
+++ app/Http/Resources/GrantsAndProjectsForProfileResource.php
@@ -21,9 +21,13 @@ class GrantsAndProjectsForProfileResource extends JsonResource
      */
     public function toArray($request)
     {
+        $grantStage = ($this->grantsAndProjectsForProfileData->grant_stage_id === 8) ? "archive" : "current";
+        $addStringForTitle = ($this->grantsAndProjectsForProfileData->grant_stage_id === 8) ? " (Archive)" : "";
+
         return [
             'id' => $this->grantsAndProjectsForProfileData->id,
-            'title' => $this->grantsAndProjectsForProfileData->title,
+            'title' => $this->grantsAndProjectsForProfileData->title . $addStringForTitle,
+            'stage' => $grantStage,
             'projects' => ProjectsResource::collection($this->grantsAndProjectsForProfileData->customerProjects)
         ];
     }
