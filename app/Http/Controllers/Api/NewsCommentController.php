4cdb23d9d4b9145a77cb1d7b2038f179fbe3d6d2
vadim.ustinovich <vadim.ustinovich@softlex.pro>
2021-02-08 14:32:05 +0300

    PWW-167 made creation NewsComments

--- app/Http/Controllers/Api/NewsCommentController.php
+++ app/Http/Controllers/Api/NewsCommentController.php
@@ -3,13 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Http\Requests\CreateCommentToNewsRequest;
+use App\Services\NewsCommentService;
+use App\Http\Requests\CommentToNewsStoreRequest;
+use Throwable;
 
 class NewsCommentController extends Controller
 {
-    public function addCommentToNews(CreateCommentToNewsRequest $request)
+    protected $newsCommentService;
+
+    public function __construct(NewsCommentService $newsCommentService)
     {
-        return 'hello';
+        $this->newsCommentService = $newsCommentService;
+    }
+
+    public function addCommentToNews(CommentToNewsStoreRequest $request)
+    {
+        try {
+            $comment = $this->newsCommentService->store($request);
+            $response = $comment->id;
+            $message = 'Comment saved successfully.';
+        } catch (Throwable $exception) {
+            $response = null;
+            $message = 'There was an error while saving the comment.';
+            report($exception);
+        }
+
+        return response()->json(['message' => $message, 'response' => $response]);
     }
 }
