fb78857ab4b6dea06c5db850685dce021e2d0507
vadim.ustinovich <vadim.ustinovich@softlex.pro>
2021-03-01 10:55:10 +0300

    PWW-224 fix link go to view in projects

--- config/custom.php
+++ config/custom.php
@@ -4,5 +4,5 @@ return [
     'storagePath' => 'storage/',
     'frontendUrlGetNews' => env('FRONTEND_URL_GET_NEWS', 'https://waqf.softlex.pro/news/'),
     'backendUrl' => env('APP_URL', 'https://waqf-admin.softlex.pro'),
-    'frontendUrlGetProject' => env('FRONTEND_URL_GET_PROJECT', 'https://waqf.softlex.pro/projects/'),
+    'frontendUrlGetProject' => env('FRONTEND_URL_GET_PROJECT', 'https://waqf.softlex.pro/projects/project/'),
 ];
